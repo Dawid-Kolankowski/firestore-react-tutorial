8fff5857cc06cdb5f68e8b376c665a0a7487dc25
Dawid <nexog1997@gmail.com>
2021-01-12 23:45:24 +0100

    User context refactor

--- src/components/CurrentUser.jsx
+++ src/components/CurrentUser.jsx
@@ -9,7 +9,7 @@ const CurrentUser = ({ displayName, photoURL, email, createdAt, children }) => {
         <div className="CurrentUser--information">
           <h2>{displayName}</h2>
           <p className="email">{email}</p>
-          <p className="created-at">{moment(createdAt).calendar()}</p>
+          <p className="created-at">{moment(createdAt.toDate()).calendar()}</p>
         </div>
       </div>
       <div>
@@ -19,12 +19,5 @@ const CurrentUser = ({ displayName, photoURL, email, createdAt, children }) => {
     </section>
   );
 };
-
-// CurrentUser.defaultProps = {
-//   displayName: "Bill Murray",
-//   email: "billmurray@mailinator.com",
-//   photoURL: "https://www.fillmurray.com/300/300",
-//   createdAt: new Date(),
-// };
 
 export default CurrentUser;
